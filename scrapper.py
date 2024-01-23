41bbf3e94da1a7be2a94c5857c46934dde93c85c
Noe Ulises Machaca Chambilla <115515737+ratatouille99@users.noreply.github.com>
2024-01-23 01:36:11 -0500

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,11 +8,14 @@ URL = 'https://www.imdb.com/calendar/?region=MX'
 #. si el archivo no existe crarlo
 #- si existe obtener su contenido
 #obtener informacion
+# - nombre
+# - categorias 
+# - repartos
 #generar archivo csv
 
 def get_imdb_content():
     headers = {
-        'User-Agent' : 'Mozila/5.0'
+        'User-Agent' : 'Mozilla/5.0'
     }
     
     response = requests.get(URL, headers = headers)
@@ -51,10 +55,52 @@ def get_local_imdb_content():
     
     return content
 
+def create_movie(tag):
+    main_div = tag.find('div', {'class' : 'ipc-metadata-list-summary-item__c'})
+        
+    name =  main_div.div.a.text
+    ul_categories = main_div.find('ul', {
+        'class' : 'ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--no-wrap ipc-inline-list--inline ipc-metadata-list-summary-item__tl base'
+    })
+        
+    ul_cast = main_div.find('ul', {
+        'class' : 'ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--no-wrap ipc-inline-list--inline ipc-metadata-list-summary-item__stl base'
+    })
+       
+    casts = None 
+    categories = [category.span.text for category in ul_categories.find_all('li')]
+    
+    if ul_cast:
+        casts = [cast.span.text for cast in ul_cast.find_all('li')]
+
+    return (name, categories, casts)
+
 def main():
     content = get_local_imdb_content()
-    print(content)
+    
+    soup = BeautifulSoup(content, 'html.parser')
+   
+    li_tags = soup.find_all('li', {
+        'data-testid': 'coming-soon-entry',
+        'class': 'ipc-metadata-list-summary-item ipc-metadata-list-summary-item--click sc-8c2b7f1f-0 eWVqBf'
+    })
     
     
+    movies = []
+    for tag in li_tags:
+        movie = create_movie(tag)   
+        movies.append(movie)
+           
+    with open('movies.csv', 'w') as file:
+        writer = csv.writer(file, delimiter="-")
+        writer.writerow(['name','categories','cast'])
+        
+        for movie in movies:
+            writer.writerow([
+                movie[0],
+                ",".join(movie[1]),
+                ",".join(movie[2]),
+            ])
+           
 if __name__ == '__main__':
     main()
